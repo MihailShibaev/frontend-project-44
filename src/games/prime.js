c20612525d761f9d4d6cf65fb1d707930ca05011
shibaev <shibaev.mihail@hexly.ru>
2022-12-04 13:18:50 +0300

    eslint config fix and linting prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,9 +3,9 @@ import randomIntFromInterval from '../random.js';
 
 const gameInvitation = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
-  if(num<=1){return false;}
-  for (let i=2; i< num; i+=1){
-    if (num %i === 0) return false;
+  if (num <= 1) { return false; }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
   }
   return true;
 };
